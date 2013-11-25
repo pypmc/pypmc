74833210bf36ea5d26d2757e2c26d1ebcd7fdd06
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2013-11-25 17:46:00 +0100

    [pypmc] Fix setup.py and versioning

--- pypmc/__init__.py
+++ pypmc/__init__.py
@@ -1,4 +1,2 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-__version__ = '0.1'
--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+# set the version number
+with open('pypmc/version.py') as f:
+    exec(f.read())
+
+n = 'pypmc'
+
 setup(
-    name='pypmc',
+    name=n,
+    packages=[n],
+    version=__version__,
     author='Frederik Beaujean',
     author_email='Frederik.Beaujean@lmu.de',
     license='GPLv2',
