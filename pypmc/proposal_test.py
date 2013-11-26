9d04848675b2a0b09b396140a81e8738dc99504c
Frederik Beaujean <beaujean@mpp.mpg.de>
2013-11-26 10:30:27 +0100

    (wip)[pypmc] Add proposal skeleton

--- pypmc/proposal_test.py
+++ pypmc/proposal_test.py
@@ -3,7 +3,20 @@
 """
 
 from proposal import *
+import numpy as np
 import unittest
 
-class TestProposal(unittest.TestCase):
+class TestGaussian(unittest.TestCase):
+    def test_evaluate(self):
+        sigma = np.matrix([[0.01, 0.003],[0.003, 0.0025]])
+        t = MultivariateStudentT(mu=np.zeros(2), sigma=sigma)
+
+        x = np.array([4.3, 1.1])
+        y = np.array([4.35, 1.2])
+
+        target = 1.30077135
+        self.assertAlmostEqual(t.evaluate(x, y), 0, delta=1e-8)
+        self.assertAlmostEqual(t.evaluate(y, x), 0, delta=1e-8)
+
+class TestStudentT(unittest.TestCase):
     pass
