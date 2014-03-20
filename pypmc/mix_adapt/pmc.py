2fd3b597a3773e5290b5fafa7a0212802a25f9de
Stephan Jahn <gu92biv@mytum.de>
2014-03-20 21:44:00 +0100

    [mix_adapt/pmc] bugfix, cleanup and added Rao-Blackwell sensitive test case
    o removed unneccessary definitions from pmc_test (i.e. target, sampler)
    o added samples from overlapping components such that the Rao-Blackwell
      option matters
    o calculate_rho_.. and application of ``mincount`` criterion separated
    o during the updates perform check "weights is None" only once

--- pypmc/mix_adapt/pmc.py
+++ pypmc/mix_adapt/pmc.py
@@ -10,7 +10,7 @@ from math import exp as _exp
 from copy import deepcopy as _cp
 from ..tools._regularize import regularize
 
-def gaussian_pmc(samples, density, weights=None, latent=None, rb=True, mincount=0, copy=True, ):
+def gaussian_pmc(samples, density, weights=None, latent=None, rb=True, mincount=0, copy=True):
     '''Adapt a mixture ``density`` using the (M-)PMC algorithm according
     to [Cap+08]_.
 
@@ -66,6 +66,32 @@ def gaussian_pmc(samples, density, weights=None, latent=None, rb=True, mincount=
         Otherwise, ``density`` is overwritten by the adapted density.
 
     '''
+    need_renormalize = False
+
+    if copy:
+        density = _cp(density)
+
+    def calculate_rho_rb():
+        rho = _np.zeros(( len(samples),len(density.components) ))
+        for k in range(len(density.components)):
+            if density.weights[k] == 0.:
+                # skip unneccessary calculation
+                continue
+            for n, sample in enumerate(samples):
+                rho[n, k]  = _exp(density.components[k].evaluate(sample)) * density.weights[k]
+                # + "tiny" --> avoid division by zero
+                rho[n, k] /= _exp(density.evaluate(sample)) + _np.finfo('d').tiny
+        return rho
+
+    def calculate_rho_non_rb():
+        rho = _np.zeros(( len(samples),len(density.components) ))
+        for k in range(len(density.components)):
+            if density.weights[k] == 0.:
+                # skip unneccessary calculation
+                continue
+            rho[latent==k,k] = 1.
+        return rho
+
     if weights is not None:
         weights = _np.asarray(weights)
         assert len(weights.shape) == 1, 'Weights must be one-dimensional.'
@@ -73,91 +99,74 @@ def gaussian_pmc(samples, density, weights=None, latent=None, rb=True, mincount=
             "Number of weights (%s) does not match the number of samples (%s)." % (len(weights), len(samples))
         normalized_weights = weights / weights.sum()
 
-    def calculate_rho_rb():
-        # if a component is pruned, the other weights must be renormalized
-        need_renormalize = False
-        rho = _np.zeros(( len(samples),len(density.components) ))
-        for k in range(len(density.components)):
-            if density.weights[k] == 0.:
-                # skip unneccessary calculation
-                pass
-            elif count[k] < mincount:
-                density.weights[k] = 0.
-                need_renormalize = True
-                print("Component %i died because of too few (%i) samples." %(k, count[k]))
-            else:
-                for n, sample in enumerate(samples):
-                    rho[n, k]  = _exp(density.components[k].evaluate(sample)) * density.weights[k]
-                    # + "tiny" --> avoid division by zero
-                    rho[n, k] /= _exp(density.evaluate(sample)) + _np.finfo('d').tiny
-        return rho, need_renormalize
-
-    def calculate_rho_non_rb():
-        # if a component is pruned, the other weights must be renormalized
-        need_renormalize = False
-        rho = _np.zeros(( len(samples),len(density.components) ), dtype=bool)
-        for k in range(len(density.components)):
-            if density.weights[k] == 0.:
-                # skip unneccessary calculation
-                continue
-            if count[k] < mincount:
-                density.weights[k] = 0.
-                need_renormalize = True
-                print("Component %i died because of too few (%i) samples." %(k, count[k]))
-            rho[latent==k,k] = True
-        return rho, need_renormalize
-
-    if copy:
-        density = _cp(density)
-
     if latent is None:
         if mincount > 0:
             raise ValueError('`mincount` must be 0 if `latent` is not provided!')
         if not rb:
             raise ValueError('`rb` must be True if `latent` is not provided!')
-        count = _np.ones(len(density.components))
-        rho, need_renormalize = calculate_rho_rb()
-
+        rho = calculate_rho_rb()
 
     else: # if latent is not None
-        count = _np.histogram(latent, bins=len(density.components), range=(0,len(density.components)))[0]
         if rb:
-            rho, need_renormalize = calculate_rho_rb()
+            rho = calculate_rho_rb()
         else:
-            rho, need_renormalize = calculate_rho_non_rb()
+            rho = calculate_rho_non_rb()
+        # prune components with less samples than ``mincount`` AFTER rho has been calculated
+        count = _np.histogram(latent, bins=len(density.components), range=(0,len(density.components)))[0]
+        for k in range(len(density.components)):
+            if count[k] < mincount:
+                density.weights[k] = 0.
+                # when a component is pruned, the other weights must be renormalized
+                need_renormalize = True
+                print("Component %i died because of too few (%i) samples." %(k, count[k]))
 
     # -------------- update equations according to (14) in [Cap+08] --------------
 
-    # new component weights
-    if weights is not None:
-        alpha = _np.einsum('n,nk->k', normalized_weights, rho)
-    else:
-        alpha = _np.einsum('nk->k', rho) / len(samples)
-    inv_alpha = 1. / regularize(alpha)
-
-    # new means
-    if weights is not None:
-        mu = _np.einsum('n,nk,ni->ki', normalized_weights, rho, samples)
-    else:
-        mu = _np.einsum('nk,ni->ki', rho, samples) / len(samples)
-    mu = _np.einsum('ki,k->ki', mu, inv_alpha)
-
-    # new covars
-    cov = _np.empty(( len(mu),len(samples[0]),len(samples[0]) ))
+    # allocate memory for covariances (other memory is allocated on demand)
+    cov = _np.empty(( len(density.components),len(samples[0]),len(samples[0]) ))
     x_minus_mu = _np.empty((len(samples), len(samples[0])))
-    for k in range(len(density.components)):
-        if density.weights[k] == 0.:
-            # skip unneccessary calculation
-            continue
-        else:
+
+
+    if weights is not None:
+
+        # new component weights
+        alpha = _np.einsum('n,nk->k', normalized_weights, rho)
+        inv_alpha = 1. / regularize(alpha)
+
+        # new means
+        mu = _np.einsum('n,nk,ni->ki', normalized_weights, rho, samples)
+        mu = _np.einsum('ki,k->ki', mu, inv_alpha)
+
+        # new covars
+        for k in range(len(density.components)):
+            if density.weights[k] == 0.:
+                # skip unneccessary calculation
+                continue
             x_minus_mu[:] = samples
             x_minus_mu -= mu[k]
-            if weights is not None:
-                _np.einsum('n,n,ni,nj->ij', normalized_weights, rho[:,k], x_minus_mu, x_minus_mu, out=cov[k])
-            else:
-                _np.einsum('n,ni,nj->ij', rho[:,k], x_minus_mu, x_minus_mu, out=cov[k])
+            _np.einsum('n,n,ni,nj->ij', normalized_weights, rho[:,k], x_minus_mu, x_minus_mu, out=cov[k])
             cov[k] *= inv_alpha[k]
-    if weights is None:
+
+    else: # if weights is None
+
+        # new component weights
+        alpha = _np.einsum('nk->k', rho) / len(samples)
+        inv_alpha = 1. / regularize(alpha)
+
+        # new means
+        mu = _np.einsum('nk,ni->ki', rho, samples) / len(samples)
+        mu = _np.einsum('ki,k->ki', mu, inv_alpha)
+
+        # new covars
+        for k in range(len(density.components)):
+            if density.weights[k] == 0.:
+                # skip unneccessary calculation
+                continue
+            x_minus_mu[:] = samples
+            x_minus_mu -= mu[k]
+            _np.einsum('n,ni,nj->ij', rho[:,k], x_minus_mu, x_minus_mu, out=cov[k])
+            cov[k] *= inv_alpha[k]
+
         cov /= len(samples)
 
     # ----------------------------------------------------------------------------
@@ -179,6 +188,7 @@ def gaussian_pmc(samples, density, weights=None, latent=None, rb=True, mincount=
                 print("Could not update component %i --> weight is set to zero." %k)
                 component.update(old_mu, old_sigma)
                 density.weights[k] = 0.
+                # when a component is pruned, the other weights must be renormalized
                 need_renormalize = True
     if need_renormalize:
         density.normalize()
--- pypmc/mix_adapt/pmc_test.py
+++ pypmc/mix_adapt/pmc_test.py
@@ -3,117 +3,96 @@
 '''
 
 from .pmc import *
-from ..sampler import importance_sampling
 from .. import density
-from ..tools._probability_densities import normalized_pdf_gauss
-from math import log
 import numpy as np
 import unittest
+#TODO: compare with pmclib (Kilbinger et. al.)
 
-rng_seed  = 295627184
-rng_steps = 20
+class TestPMCNoOverlap(unittest.TestCase):
+    # proposal density
+    mu1 = np.array( [ 10., -1.0, 8.0] )
+    mu2 = np.array( [-10.,  7.4, 0.5] )
 
-# ------------------- define the weighted samples' context  -------------------
+    cov1 = np.array([[1.15 , 0.875, 0.0],
+                     [0.875, 0.75 ,-0.2],
+                     [0.0  ,-0.2  , 1.1]])
 
-mu1 = np.array( [ 10., -1.0, 8.0] )
-mu2 = np.array( [-10.,  7.4, 0.5] )
+    cov2 = np.array([[1.0  , 0.01 , 0.1],
+                     [0.01 , 0.75 , 0.0],
+                     [0.1  , 0.0  , 2.1]])
 
-cov1 = np.array([[1.15 , 0.875, 0.0],
-                 [0.875, 0.75 ,-0.2],
-                 [0.0  ,-0.2  , 1.1]])
+    inv_cov1 = np.linalg.inv(cov1)
+    inv_cov2 = np.linalg.inv(cov2)
 
-cov2 = np.array([[1.0  , 0.01 , 0.1],
-                 [0.01 , 0.75 , 0.0],
-                 [0.1  , 0.0  , 2.1]])
+    gauss1 = density.gauss.Gauss(mu1+.001, cov1+.001)
+    gauss2 = density.gauss.Gauss(mu2-.005, cov2-.005)
+    component_weights = np.array( (.7, .3) )
+    prop = density.mixture.MixtureDensity((gauss1,gauss2), component_weights)
 
-inv_cov1 = np.linalg.inv(cov1)
-inv_cov2 = np.linalg.inv(cov2)
+    # samples, weights and latent variables
+    latent = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1])
+    weights = np.array([12.89295915,  12.89372694,  12.89781423,  12.79548829,
+                        12.89397248,  12.88642498,  12.89875608,  12.8977244 ,
+                        12.8834032 ,  12.81344527,  12.8966767 ,  12.89319812,
+                        20.02787201,  19.89550322,  19.81661548,  19.9733172 ,
+                        19.81867511,  19.81555008,  19.83955669,  19.83352245])
+    samples = np.array([[  9.7070033 ,  -1.14093259,   7.79492513],
+                        [  9.56875908,  -1.3205348 ,   7.3705522 ],
+                        [ 10.53728461,  -0.93171182,   8.76279014],
+                        [  9.80289836,  -1.15107748,   9.27682257],
+                        [  8.91717444,  -1.62000575,   7.60676764],
+                        [  9.55705421,  -1.65785994,   9.4330834 ],
+                        [ 10.90155376,  -0.42097835,   7.64481752],
+                        [ 11.06838483,  -0.65188323,   8.69936008],
+                        [  8.50673184,  -2.45559049,   8.62152455],
+                        [ 10.8097935 ,  -0.33471831,   8.60497435],
+                        [ 10.46129646,  -1.04132199,   9.04460811],
+                        [ 10.23040728,  -0.63621386,   6.48880065],
+                        [-10.76972316,   8.23669361,   2.06283074],
+                        [-11.26019812,   7.03488615,  -0.87321151],
+                        [ -9.99070915,   6.83422119,   0.28846651],
+                        [ -9.39271812,   7.08741571,   1.91672609],
+                        [-10.98814859,   7.55372701,   0.48618477],
+                        [ -9.60983136,   6.24723833,   1.06241101],
+                        [-10.61752466,   7.39052825,   1.17726011],
+                        [-10.4898097 ,   7.48668861,  -2.41443733]])
 
-# target density
-target_abundances = np.array( (.6, .4) )
-log_target = lambda x:   log(target_abundances[0] * (normalized_pdf_gauss(x, mu1, inv_cov1)) + \
-                             target_abundances[1] * (normalized_pdf_gauss(x, mu2, inv_cov2)) ) \
-                       + log(15.) # break normalization
-
-# proposal density
-gauss1 = density.gauss.Gauss(mu1+.001, cov1+.001)
-gauss2 = density.gauss.Gauss(mu2-.005, cov2-.005)
-proposal_abundances = np.array( (.7, .3) )
-prop = density.mixture.MixtureDensity((gauss1,gauss2), proposal_abundances)
-
-# -----------------------------------------------------------------------------
-
-latent = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1])
-weights = np.array([12.89295915,  12.89372694,  12.89781423,  12.79548829,
-                    12.89397248,  12.88642498,  12.89875608,  12.8977244 ,
-                    12.8834032 ,  12.81344527,  12.8966767 ,  12.89319812,
-                    20.02787201,  19.89550322,  19.81661548,  19.9733172 ,
-                    19.81867511,  19.81555008,  19.83955669,  19.83352245])
-samples = np.array([[  9.7070033 ,  -1.14093259,   7.79492513],
-                    [  9.56875908,  -1.3205348 ,   7.3705522 ],
-                    [ 10.53728461,  -0.93171182,   8.76279014],
-                    [  9.80289836,  -1.15107748,   9.27682257],
-                    [  8.91717444,  -1.62000575,   7.60676764],
-                    [  9.55705421,  -1.65785994,   9.4330834 ],
-                    [ 10.90155376,  -0.42097835,   7.64481752],
-                    [ 11.06838483,  -0.65188323,   8.69936008],
-                    [  8.50673184,  -2.45559049,   8.62152455],
-                    [ 10.8097935 ,  -0.33471831,   8.60497435],
-                    [ 10.46129646,  -1.04132199,   9.04460811],
-                    [ 10.23040728,  -0.63621386,   6.48880065],
-                    [-10.76972316,   8.23669361,   2.06283074],
-                    [-11.26019812,   7.03488615,  -0.87321151],
-                    [ -9.99070915,   6.83422119,   0.28846651],
-                    [ -9.39271812,   7.08741571,   1.91672609],
-                    [-10.98814859,   7.55372701,   0.48618477],
-                    [ -9.60983136,   6.24723833,   1.06241101],
-                    [-10.61752466,   7.39052825,   1.17726011],
-                    [-10.4898097 ,   7.48668861,  -2.41443733]])
-
-## ``latent``, ``weights`` and ``samples`` have been obtained using the following code:
-#np.random.mtrand.seed(rng_seed)
-#sam = sampler.ImportanceSampler(log_target, prop, rng=np.random.mtrand)
-#new_origins = sam.run(rng_steps, trace=True)
-#new_weighted_samples = sam.history[-1]
-
-class TestPMC(unittest.TestCase):
     def test_invalid_usage(self):
         self.assertRaisesRegexp(ValueError, r'["\'` ]*rb["\'` ]*must.*["\' `]*True["\'` ]* if["\'` ]*latent["\'` ]*.*not',
-                                gaussian_pmc, samples, prop, weights, rb=False)
+                                gaussian_pmc, self.samples, self.prop, self.weights, rb=False)
         self.assertRaisesRegexp(ValueError, r'["\'` ]*mincount["\'` ]*must.*["\' `]*[0(zero)]["\'` ]* if["\'` ]*latent["\'` ]*.*not',
-                                gaussian_pmc, samples, prop, weights, mincount=10)
+                                gaussian_pmc, self.samples, self.prop, self.weights, mincount=10)
         self.assertRaisesRegexp(ValueError, r'(["\'` ]*mincount["\'` ]*must.*["\' `]*[0(zero)]["\'` ]* if["\'` ]*latent["\'` ]*.*not)' + \
                                             r'|' + \
                                             r'(["\'` ]*rb["\'` ]*must.*["\' `]*True["\'` ]* if["\'` ]*latent["\'` ]*.*not)',
-                                gaussian_pmc, samples, prop, weights, mincount=10, rb=False)
+                                gaussian_pmc, self.samples, self.prop, self.weights, mincount=10, rb=False)
 
     def test_mincount_and_copy(self):
-        prop_weights = prop.weights.copy()
-        prop_mu0     = prop.components[0].mu.copy()
-        prop_mu1     = prop.components[1].mu.copy()
-        prop_cov0    = prop.components[0].sigma.copy()
-        prop_cov1    = prop.components[1].sigma.copy()
+        self.prop_weights = self.prop.weights.copy()
+        self.prop_mu0     = self.prop.components[0].mu.copy()
+        self.prop_mu1     = self.prop.components[1].mu.copy()
+        self.prop_cov0    = self.prop.components[0].sigma.copy()
+        self.prop_cov1    = self.prop.components[1].sigma.copy()
 
-        adapted_prop_no_die_rb     = gaussian_pmc(samples, prop, weights, latent, mincount=8, rb=True )
-        adapted_prop_die_rb        = gaussian_pmc(samples, prop, weights, latent, mincount=9, rb=True )
-        adapted_prop_no_die_no_rb  = gaussian_pmc(samples, prop, weights, latent, mincount=8, rb=False)
-        adapted_prop_die_no_rb     = gaussian_pmc(samples, prop, weights, latent, mincount=9, rb=False)
+        adapted_prop_no_die_rb     = gaussian_pmc(self.samples, self.prop, self.weights, self.latent, mincount=8, rb=True )
+        adapted_prop_die_rb        = gaussian_pmc(self.samples, self.prop, self.weights, self.latent, mincount=9, rb=True )
+        adapted_prop_no_die_no_rb  = gaussian_pmc(self.samples, self.prop, self.weights, self.latent, mincount=8, rb=False)
+        adapted_prop_die_no_rb     = gaussian_pmc(self.samples, self.prop, self.weights, self.latent, mincount=9, rb=False)
 
         self.assertNotEqual(adapted_prop_no_die_rb.weights[1]   , 0.)
         self.assertEqual   (adapted_prop_die_rb.weights[1]      , 0.)
         self.assertNotEqual(adapted_prop_no_die_no_rb.weights[1], 0.)
         self.assertEqual   (adapted_prop_die_no_rb.weights[1]   , 0.)
 
-        # the proposal should not have been touched --> expect exact equality
-        np.testing.assert_equal(prop.weights            , prop_weights)
-        np.testing.assert_equal(prop.components[0].mu   , prop_mu0    )
-        np.testing.assert_equal(prop.components[1].mu   , prop_mu1    )
-        np.testing.assert_equal(prop.components[0].sigma, prop_cov0   )
-        np.testing.assert_equal(prop.components[1].sigma, prop_cov1   )
+        # the self.proposal should not have been touched --> expect exact equality
+        np.testing.assert_equal(self.prop.weights            , self.prop_weights)
+        np.testing.assert_equal(self.prop.components[0].mu   , self.prop_mu0    )
+        np.testing.assert_equal(self.prop.components[1].mu   , self.prop_mu1    )
+        np.testing.assert_equal(self.prop.components[0].sigma, self.prop_cov0   )
+        np.testing.assert_equal(self.prop.components[1].sigma, self.prop_cov1   )
 
     def test_gaussian_pmc_with_origin(self):
-        #TODO: compare with pmclib (Kilbinger et. al.)
-        adapted_prop = gaussian_pmc(samples, prop, weights, latent)
+        adapted_prop = gaussian_pmc(self.samples, self.prop, self.weights, self.latent)
 
         adapted_comp_weights = adapted_prop.weights
         adapted_mu1          = adapted_prop.components[0].mu
@@ -139,8 +118,7 @@ class TestPMC(unittest.TestCase):
         np.testing.assert_allclose(adapted_sigma2      , pmc_sigma2      )
 
     def test_gaussian_pmc_without_origin(self):
-        #TODO: compare with pmclib (Kilbinger et. al.)
-        adapted_prop = gaussian_pmc(samples, prop, weights)
+        adapted_prop = gaussian_pmc(self.samples, self.prop, self.weights)
 
         adapted_comp_weights = adapted_prop.weights
         adapted_mu1          = adapted_prop.components[0].mu
@@ -166,7 +144,7 @@ class TestPMC(unittest.TestCase):
         np.testing.assert_allclose(adapted_sigma2      , pmc_sigma2      )
 
     def test_unweighted(self):
-        adapted_prop = gaussian_pmc(samples, prop, weights=None)
+        adapted_prop = gaussian_pmc(self.samples, self.prop, weights=None)
 
         adapted_comp_weights = adapted_prop.weights
         adapted_mu1          = adapted_prop.components[0].mu
@@ -191,5 +169,138 @@ class TestPMC(unittest.TestCase):
         np.testing.assert_allclose(adapted_sigma1      , pmc_sigma1      )
         np.testing.assert_allclose(adapted_sigma2      , pmc_sigma2      )
 
-#TODO: create test case such that the result depends on Rao Blackwellized on/off
-#TODO: test if rb is really switched on/off by argument "rb" even if "origin" is provided
+class TestPMCWithOverlap(unittest.TestCase):
+    # proposal density
+    mu1 = np.array([ 10.5,   1.1,   8.0])
+    mu2 = np.array([ 10.3,   1.4,   7.8])
+
+    cov1 = np.array([[1.15 , 0.875, 0.0],
+                     [0.875, 0.75 ,-0.2],
+                     [0.0  ,-0.2  , 1.1]])
+
+    cov2 = np.array([[1.0  , 0.01 , 0.1],
+                     [0.01 , 0.75 , 0.0],
+                     [0.1  , 0.0  , 2.1]])
+
+    inv_cov1 = np.linalg.inv(cov1)
+    inv_cov2 = np.linalg.inv(cov2)
+
+    gauss1 = density.gauss.Gauss(mu1+.001, cov1+.001)
+    gauss2 = density.gauss.Gauss(mu2-.005, cov2-.005)
+    component_weights = np.array( (.7, .3) )
+    prop = density.mixture.MixtureDensity((gauss1,gauss2), component_weights)
+
+    # samples and latent variables (no weights here)
+    latent = np.array([0, 1, 1, 0, 0, 1, 0, 0, 1, 0])
+    samples = np.array([[  8.25176666,  -0.46218162,   6.38766958],
+                        [ 10.30030254,   0.7211386 ,   8.75618216],
+                        [  9.80807539,   2.0664736 ,   8.3028291 ],
+                        [ 11.38801874,   1.90190379,   7.76177193],
+                        [ 11.06700609,   1.52600855,   8.82273509],
+                        [ 10.97130581,   0.23669273,   6.89884423],
+                        [ 10.2306073 ,   0.76789268,   8.30328072],
+                        [ 10.55012467,   1.26067336,   6.83958086],
+                        [  9.44298062,   1.47722829,   8.32750139],
+                        [ 11.74685298,   2.17160491,   6.83467116]])
+
+
+    def test_with_rb(self):
+        adapted_prop = gaussian_pmc(self.samples, self.prop, latent=self.latent, rb=True)
+
+        adapted_comp_weights = adapted_prop.weights
+        adapted_mu1          = adapted_prop.components[0].mu
+        adapted_mu2          = adapted_prop.components[1].mu
+        adapted_sigma1       = adapted_prop.components[0].sigma
+        adapted_sigma2       = adapted_prop.components[1].sigma
+
+        # the values that should be obtained by the pmc algorithm
+        pmc_comp_weights = np.array( [  6.52555056,  3.47444944]) / 10.
+        pmc_mu1          = np.array( [ 10.4920552 ,   1.11735768,   7.66532057])
+        pmc_mu2          = np.array( [ 10.15717878,   1.25949764,   7.83278897])
+        pmc_sigma1       = np.array([[ 11.54562717,   8.79011745,   3.21516184],
+                                     [  8.79011745,   6.8816927 ,   1.41711901],
+                                     [  3.21516184,   1.41711901,   8.52875733]]) / 10.
+        pmc_sigma2       = np.array([[  4.92021176,  -3.00590244,  -3.60721614],
+                                     [ -3.00590244,   5.56509117,   4.1113181 ],
+                                     [ -3.60721614,   4.1113181 ,   4.9491569 ]]) / 10.
+
+        np.testing.assert_allclose(adapted_comp_weights, pmc_comp_weights)
+        np.testing.assert_allclose(adapted_mu1         , pmc_mu1         )
+        np.testing.assert_allclose(adapted_mu2         , pmc_mu2         )
+        np.testing.assert_allclose(adapted_sigma1      , pmc_sigma1      )
+        np.testing.assert_allclose(adapted_sigma2      , pmc_sigma2      )
+
+    def test_non_rb(self):
+        adapted_prop = gaussian_pmc(self.samples, self.prop, latent=self.latent, rb=False)
+
+        adapted_comp_weights = adapted_prop.weights
+        adapted_mu1          = adapted_prop.components[0].mu
+        adapted_mu2          = adapted_prop.components[1].mu
+        adapted_sigma1       = adapted_prop.components[0].sigma
+        adapted_sigma2       = adapted_prop.components[1].sigma
+
+        # the values that should be obtained by the pmc algorithm
+        pmc_comp_weights = np.array([0.6, 0.4])
+        pmc_mu1          = np.array( [ 10.53906274,   1.19431695,   7.49161822])
+        pmc_mu2          = np.array( [ 10.13066609,   1.12538331,   8.07133922])
+        pmc_sigma1       = np.array([[ 12.97533036,   9.79558822,   4.01021926],
+                                     [  9.79558822,   7.49336426,   2.38326823],
+                                     [  4.01021926,   2.38326823,   7.63180987]]) / 10.
+        pmc_sigma2       = np.array([[  3.28106932,  -3.40297223,  -2.80076755],
+                                     [ -3.40297223,   4.90657639,   2.68280842],
+                                     [ -2.80076755,   2.68280842,   4.90740244]]) / 10.
+
+        np.testing.assert_allclose(adapted_comp_weights, pmc_comp_weights)
+        np.testing.assert_allclose(adapted_mu1         , pmc_mu1         )
+        np.testing.assert_allclose(adapted_mu2         , pmc_mu2         )
+        np.testing.assert_allclose(adapted_sigma1      , pmc_sigma1      )
+        np.testing.assert_allclose(adapted_sigma2      , pmc_sigma2      )
+
+    def test_mincount_with_rb(self):
+        adapted_prop = gaussian_pmc(self.samples, self.prop, latent=self.latent, rb=True, mincount=5)
+
+        adapted_comp_weights = adapted_prop.weights
+        adapted_mu1          = adapted_prop.components[0].mu
+        adapted_mu2          = adapted_prop.components[1].mu
+        adapted_sigma1       = adapted_prop.components[0].sigma
+        adapted_sigma2       = adapted_prop.components[1].sigma
+
+        # the values that should be obtained by the pmc algorithm
+        pmc_comp_weights = np.array( [  1., 0.])
+        pmc_mu1          = np.array( [ 10.4920552 ,   1.11735768,   7.66532057])
+        pmc_mu2          = self.prop[1][0].mu
+        pmc_sigma1       = np.array([[ 11.54562717,   8.79011745,   3.21516184],
+                                     [  8.79011745,   6.8816927 ,   1.41711901],
+                                     [  3.21516184,   1.41711901,   8.52875733]]) / 10.
+        pmc_sigma2       = self.prop[1][0].sigma
+
+        np.testing.assert_allclose(adapted_comp_weights, pmc_comp_weights)
+        np.testing.assert_allclose(adapted_mu1         , pmc_mu1         )
+        np.testing.assert_allclose(adapted_mu2         , pmc_mu2         )
+        np.testing.assert_allclose(adapted_sigma1      , pmc_sigma1      )
+        np.testing.assert_allclose(adapted_sigma2      , pmc_sigma2      )
+
+
+    def test_mincount_no_rb(self):
+        adapted_prop = gaussian_pmc(self.samples, self.prop, latent=self.latent, rb=False, mincount=5)
+
+        adapted_comp_weights = adapted_prop.weights
+        adapted_mu1          = adapted_prop.components[0].mu
+        adapted_mu2          = adapted_prop.components[1].mu
+        adapted_sigma1       = adapted_prop.components[0].sigma
+        adapted_sigma2       = adapted_prop.components[1].sigma
+
+        # the values that should be obtained by the pmc algorithm
+        pmc_comp_weights = np.array([1., 0.])
+        pmc_mu1          = np.array( [ 10.53906274,   1.19431695,   7.49161822])
+        pmc_mu2          = self.prop[1][0].mu
+        pmc_sigma1       = np.array([[ 12.97533036,   9.79558822,   4.01021926],
+                                     [  9.79558822,   7.49336426,   2.38326823],
+                                     [  4.01021926,   2.38326823,   7.63180987]]) / 10.
+        pmc_sigma2       = self.prop[1][0].sigma
+
+        np.testing.assert_allclose(adapted_comp_weights, pmc_comp_weights)
+        np.testing.assert_allclose(adapted_mu1         , pmc_mu1         )
+        np.testing.assert_allclose(adapted_mu2         , pmc_mu2         )
+        np.testing.assert_allclose(adapted_sigma1      , pmc_sigma1      )
+        np.testing.assert_allclose(adapted_sigma2      , pmc_sigma2      )
