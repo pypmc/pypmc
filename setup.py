21494d41a10e09f8a77a2eb7f25dd227d075e822
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2014-08-18 19:12:57 +0200

    [setup] Fix build with setuptools
    o require setuptools
    o add ez_setup.py for bootstrapping
    o correctly install pure python code

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
 
+from codecs import open # To use a consistent encoding
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
@@ -18,13 +18,13 @@ extra_compile_args=["-Wno-unused-but-set-variable",
                     "-O3"]
 
 extensions = [ Extension('*', ['pypmc/*/*.pyx' ],
-                                                  extra_compile_args=extra_compile_args,
-                                                  include_dirs=[numpy.get_include()])
+                         extra_compile_args=extra_compile_args,
+                         include_dirs=[numpy.get_include()])
              ]
 
 setup(
     name=n,
-    packages=[n],
+    packages=find_packages(),
     version=__version__,
     author='Frederik Beaujean, Stephan Jahn',
     author_email='Frederik.Beaujean@lmu.de, stephan.jahn@mytum.de',
