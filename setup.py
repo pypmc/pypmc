810a1ad1cea49a411bb149fea96525887d700e5b
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2014-08-20 17:24:14 +0200

    [setup,doc] Add metadata in setup.py
    o add doc/abstract.txt for the long package description

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import numpy
 with open('pypmc/_version.py') as f:
     exec(f.read())
 
-n = 'pypmc'
+package_name = 'pypmc'
 
 extra_compile_args=["-Wno-unused-but-set-variable",
                     "-Wno-unused-function",
@@ -21,12 +21,18 @@ extensions = [ Extension('*', ['pypmc/*/*.pyx' ],
                          include_dirs=[numpy.get_include()])
              ]
 
+with open('doc/abstract.txt') as f:
+    long_description = f.read()
+
 setup(
-    name=n,
+    name=package_name,
     packages=find_packages(),
     version=__version__,
     author='Frederik Beaujean, Stephan Jahn',
     author_email='Frederik.Beaujean@lmu.de, stephan.jahn@mytum.de',
+    url='https://github.com/fredRos/pypmc',
+    description='A toolkit for adaptive importance sampling featuring implementations of variational Bayes and population Monte Carlo.',
+    long_description=long_description,
     license='GPLv2',
     install_requires=['numpy', 'scipy', 'cython', 'setuptools>=3.3'],
     extras_require={'testing': ['nose'], 'plotting': ['matplotlib'], 'parallelization': ['mpi4py']},
@@ -34,5 +40,18 @@ setup(
                           compiler_directives=dict(boundscheck=False, cdivision=True,
                                                    embedsignature=True,
                                                    profile=False, wraparound=False),
-                          )
+                          ),
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+                 'Operating System :: Unix',
+                 'Programming Language :: Cython',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Scientific/Engineering :: Mathematics',
+                 ],
+    platforms=['Unix'],
     )
